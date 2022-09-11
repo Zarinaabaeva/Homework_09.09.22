e32d2780b545ea21eb469f95f19d4ab90789cc80
Zarina <abaevazarina@yandex.ru>
2022-09-11 20:45:22 +0300

    task_29 solved

---
+++ task_29/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] GetArray()
+{
+    int[] numbers = new int[8];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-50, 50);
+    }
+    return numbers;  
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+PrintArray(GetArray());
