96dc4701bf72c1be2a0476148196af8bc346780b
Zarina <abaevazarina@yandex.ru>
2022-09-11 20:40:24 +0300

    task_27 solved

---
+++ task_27/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int a){
+
+    int sum = 0;
+    while(a > 0)
+    {
+       sum = sum + a % 10;
+       a = a / 10;    
+    }
+    return sum;
+}
+int result = Sum(a);
+Console.WriteLine(result);
+
