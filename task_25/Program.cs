875537ed8d777c898b0920c41cad0e3a85d84902
Zarina <abaevazarina@yandex.ru>
2022-09-11 20:31:21 +0300

    task_25 solved

---
+++ task_25/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число A: ");
+Console.WriteLine("Введите число B: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+
+int Pow(int A, int B){
+
+    int pow = A;
+
+    for(int i = 1; i < B; i++){
+        pow*= A;
+    }
+    return pow;
+
+}
+int result = Pow(A, B);
+Console.WriteLine(result);
